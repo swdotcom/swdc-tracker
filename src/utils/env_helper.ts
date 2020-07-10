dad2235676a7c0f392f939deae26b3812d5257c5
Mason McLead <mason.mclead@gmail.com>
2020-07-10 11:56:40 -0700

    Revert "referencing env var to simplify logic"

---
+++ src/utils/env_helper.ts
@@ -0,0 +1,15 @@
+export enum TrackerMode {
+  TEST = "test",
+  PROD = "prod",
+}
+
+export function getTrackerMode(): TrackerMode {
+  if (process.env.ENABLE_SWDC_TRACKER === "true") {
+    return TrackerMode.PROD;
+  }
+  return TrackerMode.TEST
+}
+
+export function isTestMode(): boolean {
+  return getTrackerMode() === TrackerMode.TEST ? true : false;
+}
