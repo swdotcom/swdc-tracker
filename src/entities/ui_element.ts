fcddea948fca9f64d7da7608bb09d1b6ff3fe6e4
Mason McLead <mason.mclead@gmail.com>
2020-07-11 00:19:26 -0700

    updating ui_element schema to include more options

--- src/entities/ui_element.ts
+++ src/entities/ui_element.ts
@@ -28,7 +28,7 @@ export class UIElement implements UIElementInterface {
 
   buildPayload() {
     return {
-      schema: "iglu:com.software/ui_element/jsonschema/1-0-1",
+      schema: "iglu:com.software/ui_element/jsonschema/1-0-2",
       data: {
         element_name: this.element_name,
         element_location: this.element_location,
