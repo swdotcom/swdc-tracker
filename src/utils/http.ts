af2e6f728ea52616b44e0833ffdccc4e63271719
xavluiz <xavluiz@gmail.com>
2020-07-26 13:57:30 -0700

    added timeout to prevnet ECONNRESET and catch exceptions

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,28 +1,33 @@
 import axios from "axios";
 
-let baseURL = "";
+// build the axios api base url
+let beApi: any = {};
 
 export function setBaseUrl(url: string) {
-  baseURL = url;
+  beApi = axios.create({
+    baseURL: url,
+    timeout: 15000 // timeout so we're not getting ECONNRESET
+  });
 }
 
 export async function get(endpoint: string, jwt?: string) {
-  let apiParams;
-
-  if(jwt) {
-    apiParams = { baseURL: baseURL, headers: { "Authorization": jwt }}
-  } else {
-    apiParams = { baseURL: baseURL }
+  if (jwt) {
+    beApi.defaults.headers.common["Authorization"] = jwt;
   }
 
-  const api = axios.create(apiParams);
-  const result = await api.get(endpoint);
+  const result = await beApi.get(endpoint).catch((e: any) => {
+    // we need to catch an error so we're not getting this for example;
+    // UnhandledPromiseRejectionWarning: Error: connect ECONNREFUSED 127.0.0.1:80
+    // at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1141:16)
+    return e;
+  });
   return result;
 }
 
 export async function post(endpoint: string, body: any, jwt: string) {
-  const api = axios.create({ baseURL: baseURL, headers: { "Authorization": jwt }});
-
-  const result = await api.post(endpoint, body);
+  beApi.defaults.headers.common["Authorization"] = jwt;
+  const result = await beApi.post(endpoint, body).catch((e: any) => {
+    return e;
+  });
   return result;
 }
