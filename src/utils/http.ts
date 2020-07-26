7e2b13c0a4cd3a9d530f1bc818f8acc28991c5e5
xavluiz <xavluiz@gmail.com>
2020-07-26 15:38:11 -0700

    updated axios client name

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // build the axios api base url
-let beApi: any = {};
+let axiosClient: any = {};
 
 export function setBaseUrl(url: string) {
-  beApi = axios.create({
+  axiosClient = axios.create({
     baseURL: url,
     timeout: 15000 // timeout so we're not getting ECONNRESET
   });
@@ -12,10 +12,10 @@ export function setBaseUrl(url: string) {
 
 export async function get(endpoint: string, jwt?: string) {
   if (jwt) {
-    beApi.defaults.headers.common["Authorization"] = jwt;
+    axiosClient.defaults.headers.common["Authorization"] = jwt;
   }
 
-  const result = await beApi.get(endpoint).catch((e: any) => {
+  const result = await axiosClient.get(endpoint).catch((e: any) => {
     // we need to catch an error so we're not getting this for example;
     // UnhandledPromiseRejectionWarning: Error: connect ECONNREFUSED 127.0.0.1:80
     // at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1141:16)
@@ -25,8 +25,8 @@ export async function get(endpoint: string, jwt?: string) {
 }
 
 export async function post(endpoint: string, body: any, jwt: string) {
-  beApi.defaults.headers.common["Authorization"] = jwt;
-  const result = await beApi.post(endpoint, body).catch((e: any) => {
+  axiosClient.defaults.headers.common["Authorization"] = jwt;
+  const result = await axiosClient.post(endpoint, body).catch((e: any) => {
     return e;
   });
   return result;
