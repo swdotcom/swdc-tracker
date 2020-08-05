df7d946f7b1ada8b2344888c43ba3d866b5e1cc1
xavluiz <xavluiz@gmail.com>
2020-08-05 10:04:51 -0700

    added tracker version and tracker id to the request header

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -8,6 +8,18 @@ export function setBaseUrl(url: string) {
     baseURL: url,
     timeout: 15000 // timeout so we're not getting ECONNRESET
   });
+
+  // set teh tracker version and ID
+  let version = process.env.npm_package_version;
+  let name = process.env.npm_package_name;
+  if (!version) {
+    const fs = require("fs");
+    const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
+    version = packageJson.version;
+    name = packageJson.name;
+  }
+  axiosClient.defaults.headers.common["X-SWDC-Tracker-Version"] = version || "";
+  axiosClient.defaults.headers.common["X-SWDC-Tracker-Id"] = name || "swdc-tracker";
 }
 
 export async function get(endpoint: string, jwt?: string) {
@@ -19,7 +31,8 @@ export async function get(endpoint: string, jwt?: string) {
     // we need to catch an error so we're not getting this for example;
     // UnhandledPromiseRejectionWarning: Error: connect ECONNREFUSED 127.0.0.1:80
     // at TCPConnectWrap.afterConnect [as oncomplete] (net.js:1141:16)
-    console.log("swdc-tracker get request error", e);
+    const msg = e.message || e.code;
+    console.log("swdc-tracker get request error", msg);
     return e;
   });
   return result;
@@ -28,7 +41,8 @@ export async function get(endpoint: string, jwt?: string) {
 export async function post(endpoint: string, body: any, jwt: string) {
   axiosClient.defaults.headers.common["Authorization"] = jwt;
   const result = await axiosClient.post(endpoint, body).catch((e: any) => {
-    console.log("swdc-tracker post request error", e);
+    const msg = e.message || e.code;
+    console.log("swdc-tracker post request error", msg);
     return e;
   });
   return result;
