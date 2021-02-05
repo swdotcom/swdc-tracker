9c2c2766f794013062ba768c0d04f5c2dab0f280
Daniel Alfaro <daniel@software.com>
2021-02-04 16:45:20 -0800

    Bump ui_element version to 1-0-5

--- src/entities/ui_element.ts
+++ src/entities/ui_element.ts
@@ -28,7 +28,7 @@ export class UIElement implements UIElementInterface {
 
   async buildPayload() {
     return {
-      schema: "iglu:com.software/ui_element/jsonschema/1-0-4",
+      schema: "iglu:com.software/ui_element/jsonschema/1-0-5",
       data: {
         element_name: this.element_name,
         element_location: this.element_location,
