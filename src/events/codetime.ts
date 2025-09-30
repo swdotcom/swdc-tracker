a348eac64efc30c852222b67f7fd261277d783b9
xavluiz <xavier@software.com>
2025-09-29 20:51:19 -0700

    update code time schema

--- src/events/codetime.ts
+++ src/events/codetime.ts
@@ -5,13 +5,17 @@ import {PluginInterface} from '../entities/plugin';
 import {AuthInterface} from '../entities/auth';
 
 // The CodeTime event
-export const codetime_schema: string = 'iglu:com.software/codetime/jsonschema/1-0-2';
+export const codetime_schema: string = 'iglu:com.software/codetime/jsonschema/1-0-3';
 
 export interface CodeTimeInterface {
   keystrokes: number;
   lines_added: number;
+  ai_lines_added: number;
+  ai_lines_reverted: number;
   lines_deleted: number;
   characters_added: number;
+  ai_characters_added: number;
+  ai_characters_reverted: number;
   characters_deleted: number;
   single_deletes: number;
   multi_deletes: number;
@@ -26,9 +30,13 @@ export interface CodeTimeInterface {
 export class CodeTime implements CodeTimeInterface {
   public keystrokes: number;
   public lines_added: number;
+  public ai_lines_added: number;
+  public ai_lines_reverted: number;
   public lines_deleted: number;
   public characters_added: number;
   public characters_deleted: number;
+  public ai_characters_added: number;
+  public ai_characters_reverted: number;
   public single_deletes: number;
   public multi_deletes: number;
   public single_adds: number;
@@ -41,9 +49,13 @@ export class CodeTime implements CodeTimeInterface {
   constructor(data: CodeTimeInterface) {
     this.keystrokes = data.keystrokes;
     this.lines_added = data.lines_added;
+    this.ai_lines_added = data.ai_lines_added;
+    this.ai_lines_reverted = data.ai_lines_reverted;
     this.lines_deleted = data.lines_deleted;
     this.characters_added = data.characters_added;
     this.characters_deleted = data.characters_deleted;
+    this.ai_characters_added = data.ai_characters_added;
+    this.ai_characters_reverted = data.ai_characters_reverted;
     this.single_deletes = data.single_deletes;
     this.multi_deletes = data.multi_deletes;
     this.single_adds = data.single_adds;
@@ -64,8 +76,12 @@ export class CodeTime implements CodeTimeInterface {
       data: {
         keystrokes: this.keystrokes,
         lines_added: this.lines_added,
+        ai_lines_added: this.ai_lines_added,
+        ai_lines_reverted: this.ai_lines_reverted,
         lines_deleted: this.lines_deleted,
         characters_added: this.characters_added,
+        ai_characters_added: this.ai_characters_added,
+        ai_characters_reverted: this.ai_characters_reverted,
         characters_deleted: this.characters_deleted,
         single_deletes: this.single_deletes,
         multi_deletes: this.multi_deletes,
